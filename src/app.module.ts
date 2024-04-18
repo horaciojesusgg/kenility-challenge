c47bf87feec99bf95ff81dbd67b9f8b7db90d39f
Horacio Gonzalez <hgonzalez@creatoriq.com>
2024-04-18 19:36:06 -0300

    add docker files

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/kenility'),
+    MongooseModule.forRoot('mongodb://db:27017/kenility'),
     OrdersModule,
     ProductsModule,
     AuthModule,
